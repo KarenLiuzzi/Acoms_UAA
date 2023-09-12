55b8dbec741436bcb179e8580b6c7dc156fb43f7
KarenLiuzzi <liuzzi.karen@gmail.com>
2023-09-12 15:59:39 -0400

    - Actualizacion de notificaciones leidas - Try except en notificaciones - Mejora en lista de notificaciones

--- notify/urls.py
+++ notify/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from notify.views import NotificationList
+from notify.views import NotificationList, actualizar_notificacion_leida
 
 app_name = "notify"
 
 urlpatterns = [
     path("notificacion/", NotificationList.as_view(), name="notificaciones"),
+    path("actualizarNotify/", actualizar_notificacion_leida, name="actualizar_notificacion"),
 ]
--- notify/views.py
+++ notify/views.py
@@ -1,10 +1,11 @@
 from typing import Any
 from django.db.models.query import QuerySet
+from django.http import JsonResponse
 from django.shortcuts import render
 
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-
+from notify.models import Notification
 from django.views.generic import ListView
 
 from swapper import load_model
@@ -26,3 +27,20 @@ class NotificationList(ListView):
     #filtramos solo las notificaciones que estan destinadas al usuario logeado
     def get_queryset(self):
         return self.request.user.notificaciones.all()
+    
+    
+    
+@login_required
+def actualizar_notificacion_leida(request):
+    
+    campo = request.GET.get('campo')
+    
+    if campo == "actualizar_notificacion":
+        id_notify = request.GET.get('id')
+        ins_notificacion = Notification.objects.get(id= id_notify)
+        ins_notificacion.read= True
+        ins_notificacion.save()
+        print(ins_notificacion)
+        
+    options= {'estado': 'ok'}
+    return JsonResponse(options, safe=False)
